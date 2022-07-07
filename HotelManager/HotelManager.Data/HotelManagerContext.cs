842d49c2798330bf17758f823dcf2f401c8fd022
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-07-07 12:05:04 +0800

    new Hotel Manager Project

---
+++ HotelManager/HotelManager.Data/HotelManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManager.Data
+{
+    public class HotelManagerContext : DbContext
+    {
+        public HotelManagerContext() : base("name=HotelManagerContext")
+        {
+
+        }
+
+
+    }
+}
---
+++ HotelManager/HotelManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelManager.Web.Startup))]
+namespace HotelManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
