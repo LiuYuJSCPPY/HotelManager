11f44290a26ff306ede4a1a106c35cda93ebb8cb
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-07-12 23:27:44 +0800

    User

--- HotelManager/HotelManager.Web/Areas/Dashboard/Controllers/DashboradUserController.cs
+++ HotelManager/HotelManager.Web/Areas/Dashboard/Controllers/DashboradUserController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HotelManager.Web.Areas.ViewModel;
 
 namespace HotelManager.Web.Areas.Dashboard.Controllers
 {
@@ -12,15 +13,17 @@ namespace HotelManager.Web.Areas.Dashboard.Controllers
     {
         private HotelManagerSingInManager _signInManager;
         private HotelManagerUserManager _userManager;
+        private HotelRoleManager _hotelRoleManager;
 
         public DashboradUserController()
         {
         }
 
-        public DashboradUserController(HotelManagerUserManager userManager, HotelManagerSingInManager signInManager)
+        public DashboradUserController(HotelManagerUserManager userManager, HotelManagerSingInManager signInManager, HotelRoleManager hotelRoleManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
+            _hotelRoleManager = hotelRoleManager;
         }
 
         public HotelManagerSingInManager SignInManager
@@ -46,11 +49,27 @@ namespace HotelManager.Web.Areas.Dashboard.Controllers
                 _userManager = value;
             }
         }
+
+        public HotelRoleManager HotelRoleManager
+        {
+            get
+            {
+                return _hotelRoleManager ?? HttpContext.GetOwinContext().Get<HotelRoleManager>();
+            }
+            private set
+            {
+                _hotelRoleManager = value;
+            }
+        }
         // GET: Dashboard/DashboradUser
         public ActionResult Index()
         {
-            var user = UserManager.Users.AsQueryable();
-            return View();
+            var model = new UserViewModel();
+            var ListUser = UserManager.Users.AsQueryable();
+
+            model.hotelUsers = ListUser.ToList();
+
+            return View(model);
         }
     }
 }
---
+++ HotelManager/HotelManager.Web/Areas/ViewModel/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using HotelManager.Model;
+using HotelManager.Web.Models;
+
+namespace HotelManager.Web.Areas.ViewModel
+{
+    public class UserViewModel
+    {
+        public IEnumerable<HotelUser> hotelUsers { get; set; }
+    }
+}
