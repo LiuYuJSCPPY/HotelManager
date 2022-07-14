d17ccd6ec7d6936d9e56668cb609501ef49a8097
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-07-14 17:15:27 +0800

    Font

---
+++ HotelManager/HotelManager.Web/WebShare/Attribute/CustomizeCacheAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManager.Web.WebShare.Attribute
+{
+    public class CustomizeCacheAttribute
+    {
+    }
+}
